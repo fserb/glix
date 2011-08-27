1af6bce6ce4e2b311d74d82f5276539251fc3256
Fernando Serboncini <fserb@fserb.com.br>
2011-08-27 10:21:40 +0200

    refactored buffer

--- buffer.js
+++ buffer.js
@@ -2,22 +2,23 @@ gnix.module.buffer = function(gl) {
   var buffers = {};
 
   gl.buffer = function(bufferName, drawMode) {
-    if (drawMode === undefined) drawMode = gl.STATIC_DRAW;
-    var b = buffers[bufferName] || (buffers[bufferName] = gl.createBuffer());
-
-    var _ = {
-      val: b,
-      data: function(array) {
-        gl.bindBuffer(gl.ARRAY_BUFFER, b);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array), drawMode);
-        return _;
-      },
-
-      bind: function() {
-        gl.bindBuffer(gl.ARRAY_BUFFER, b);
-        return _;
-      },
-    };
-    return _;
+    var b = buffers[bufferName];
+    if (!b) {
+      b = buffers[bufferName] = {
+        val: gl.createBuffer(),
+        data: function(array) {
+          if (drawMode === undefined) drawMode = gl.STATIC_DRAW;
+          gl.bindBuffer(gl.ARRAY_BUFFER, b.val);
+          gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(array), drawMode);
+          return b;
+        },
+        bind: function() {
+          gl.bindBuffer(gl.ARRAY_BUFFER, b.val);
+          return b;
+        },
+      };
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, b.val);
+    return b;
   };
 };
